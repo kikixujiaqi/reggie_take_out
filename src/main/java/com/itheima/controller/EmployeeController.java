faeeb9e1116a1813b2566343e2d41baf0d217e41
沈浔 <1464979993@qq.com>
2023-04-04 13:20:23 +0800

    完成登录功能

---
+++ src/main/java/com/itheima/controller/EmployeeController.java
@@ -0,0 +1,41 @@
+package com.itheima.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.itheima.common.R;
+import com.itheima.entity.Employee;
+import com.itheima.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+        if (emp == null) {
+            return R.error("用户不存在");
+        }
+        if (! emp.getPassword().equals(password)) {
+            return R.error("密码错误");
+        }
+        if (emp.getStatus() == 0) {
+            return R.error("该账号已被禁用");
+        }
+        return R.success(emp);
+    }
+}
---
+++ src/main/java/com/itheima/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,14 @@
+package com.itheima.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.entity.Employee;
+import com.itheima.mapper.EmployeeMapper;
+import com.itheima.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
+}
