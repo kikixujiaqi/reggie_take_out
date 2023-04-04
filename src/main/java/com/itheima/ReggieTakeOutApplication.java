7500d240a86b3af7f1f19594cf0f0c7cec7e0dea
沈浔 <1464979993@qq.com>
2023-04-04 09:55:05 +0800

    开启静态资源映射

--- src/main/java/com/itheima/ReggieTakeOutApplication.java
+++ src/main/java/com/itheima/ReggieTakeOutApplication.java
@@ -3,13 +3,17 @@ package com.itheima;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+
 @Slf4j
+@ServletComponentScan
 @SpringBootApplication
 public class ReggieTakeOutApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReggieTakeOutApplication.class, args);
-        log.info("项目启动成功");
+        log.info("项目启动成功~");
     }
 
 }
