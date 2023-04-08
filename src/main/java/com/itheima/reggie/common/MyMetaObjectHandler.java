b4e002dc12e90f8476ed460f83826896d8745f78
沈浔 <1464979993@qq.com>
2023-04-08 09:43:16 +0800

    公共字段填充

---
+++ src/main/java/com/itheima/reggie/common/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.itheima.reggie.common;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.itheima.reggie.entity.User;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        metaObject.setValue("createTime", LocalDateTime.now());
+        metaObject.setValue("createUser", BaseContext.getCurrentId());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+    }
+}
