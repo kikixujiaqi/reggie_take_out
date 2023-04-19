d68705262bf7fbc144c941c9eed6abf388a95f33
沈浔 <1464979993@qq.com>
2023-04-19 15:33:17 +0800

    创建处理category类所需的各个层

---
+++ src/main/java/com/itheima/reggie/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,14 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.entity.Category;
+import com.itheima.reggie.mapper.CategoryMapper;
+import com.itheima.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class CategoryServiceImpl extends ServiceImpl<CategoryMapper, Category> implements CategoryService {
+
+}
