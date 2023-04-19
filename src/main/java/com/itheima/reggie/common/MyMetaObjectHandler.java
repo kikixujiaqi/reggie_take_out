dfcb3d1a236857214f81aa7d596868a9c92b8398
沈浔 <1464979993@qq.com>
2023-04-19 15:53:45 +0800

    新增菜品分类

--- src/main/java/com/itheima/reggie/common/MyMetaObjectHandler.java
+++ src/main/java/com/itheima/reggie/common/MyMetaObjectHandler.java
@@ -14,13 +14,23 @@ import java.time.LocalDateTime;
 public class MyMetaObjectHandler implements MetaObjectHandler {
     @Override
     public void insertFill(MetaObject metaObject) {
-        metaObject.setValue("createTime", LocalDateTime.now());
-        metaObject.setValue("createUser", BaseContext.getCurrentId());
-    }
+        log.info("公共字段自动填充[insert]...");
+        log.info(metaObject.toString());
 
+        metaObject.setValue("createTime", LocalDateTime.now());
+        metaObject.setValue("updateTime",LocalDateTime.now());
+        metaObject.setValue("createUser",BaseContext.getCurrentId());
+        metaObject.setValue("updateUser",BaseContext.getCurrentId());
+    }
     @Override
     public void updateFill(MetaObject metaObject) {
-        metaObject.setValue("updateTime", LocalDateTime.now());
-        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+        log.info("公共字段自动填充[update]...");
+        log.info(metaObject.toString());
+
+        long id = Thread.currentThread().getId();
+        log.info("线程id为：{}",id);
+
+        metaObject.setValue("updateTime",LocalDateTime.now());
+        metaObject.setValue("updateUser",BaseContext.getCurrentId());
     }
 }
