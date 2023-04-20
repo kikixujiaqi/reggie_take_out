860732133c7b55ff15f70a14660a9b45a2844da6
沈浔 <1464979993@qq.com>
2023-04-20 19:11:56 +0800

    完成文件的上传下载功能

---
+++ src/main/java/com/itheima/reggie/controller/CommonController.java
@@ -0,0 +1,66 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping("/common")
+public class CommonController {
+
+    @Value("${reggie.path}")
+    private String basePath;
+
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file) {
+        String originFileName = file.getName();
+        String suffix = originFileName.substring(originFileName.lastIndexOf("."));
+        String fileName = UUID.randomUUID().toString() + suffix;
+
+        File dir = new File(basePath);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+
+        try {
+            file.transferTo(new File(basePath + fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return R.success(fileName);
+    }
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response) {
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+            ServletOutputStream outputStream = response.getOutputStream();
+            response.setContentType("jpeg/image");
+            int len = 0;
+            byte[] buff = new byte[1024];
+            while ((len = fileInputStream.read(buff)) != -1) {
+                outputStream.write(buff, 0, len);
+                outputStream.flush();
+            }
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
